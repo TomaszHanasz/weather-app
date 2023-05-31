8ef827319d03a8637d7d0d5cb1a148edc24801d4
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-05-31 08:38:58 -0500

    initial commit

---
+++ src/components/weatherApp/WeatherApp.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import WeatherCard from "../weatherCard/WeatherCard";
+import axios from "axios";
+import "./weatherApp.style.css";
+
+const WeatherApp = () => {
+  const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState(0);
+  const [description, setDescription] = useState("Weather");
+  const [feelsLikeTemp, setFeelsLikeTemp] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [wind, setWind] = useState(0);
+  const [searchedCity, setSearchedCity] = useState("");
+  const [icon, setIcon] = useState("");
+
+  const onChangeHandler = (e) => {
+    setSearchedCity(e.target.value);
+  };
+
+  const onClickHandler = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=355cf3bff397cfe55bf144d10da9b2d8`
+      );
+      const { data } = response;
+      setTemperature(data.main.temp);
+      setDescription(data.weather[0].description);
+      setFeelsLikeTemp(data.main.feels_like);
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setCity(data.name);
+      setIcon(data.weather[0].icon);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="weather-container">
+      {city && (
+        <WeatherCard
+          city={city}
+          humidity={humidity}
+          description={description}
+          feelsLikeTemp={feelsLikeTemp}
+          wind={wind}
+          temperature={temperature}
+          icon={icon}
+        />
+      )}
+      <input
+        placeholder="enter city name"
+        onChange={onChangeHandler}
+        value={searchedCity}
+      />
+      <button onClick={onClickHandler}>Get the weather</button>
+    </div>
+  );
+};
+
+export default WeatherApp;
---
+++ src/components/weatherCard/WeatherCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./weatherCard.style.css";
+
+const WeatherCard = (props) => {
+  const {
+    city,
+    temperature,
+    description,
+    feelsLikeTemp,
+    humidity,
+    wind,
+    icon,
+  } = props;
+
+  return (
+    <div className="weather-card">
+      <h3>{city}</h3>
+      <img src={`http://openweathermap.org/img/w/${icon}.png`} />
+      <div className="temp-box">
+        <p className="temp">{temperature}º</p>
+        <div className="description">
+          <p>{description}</p>
+          <p>Feels like: {feelsLikeTemp}º</p>
+          <p>Humidity: {humidity} %</p>
+          <p>Wind: {wind} mph</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherCard;
