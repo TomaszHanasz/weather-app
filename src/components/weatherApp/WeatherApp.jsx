4756060fa2472e16a6651e3ab766b6e7c3a2a49e
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-05-31 11:31:05 -0500

    local storage added

--- src/components/weatherApp/WeatherApp.jsx
+++ src/components/weatherApp/WeatherApp.jsx
@@ -4,13 +4,14 @@ import axios from "axios";
 import "./weatherApp.style.css";
 
 const WeatherApp = () => {
+  const lastCity = localStorage.getItem("Storage");
   const [city, setCity] = useState("");
   const [temperature, setTemperature] = useState(0);
   const [description, setDescription] = useState("");
   const [feelsLikeTemp, setFeelsLikeTemp] = useState(0);
   const [humidity, setHumidity] = useState(0);
   const [wind, setWind] = useState(0);
-  const [searchedCity, setSearchedCity] = useState("");
+  const [searchedCity, setSearchedCity] = useState(lastCity);
   const [icon, setIcon] = useState("");
 
   const onChangeHandler = (e) => {
@@ -25,6 +26,7 @@ const WeatherApp = () => {
 
   const onClickHandler = async () => {
     try {
+      localStorage.setItem("Storage", searchedCity);
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=355cf3bff397cfe55bf144d10da9b2d8`
       );
